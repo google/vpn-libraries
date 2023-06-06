823d6558f492a554fffca39da07766dc2edbbca6
Googler <noreply@google.com>
2023-06-06 08:27:33 -0700

    Internal change
    PiperOrigin-RevId: 538190178

---
+++ common/cpp/public_metadata/public_metadata.cc
@@ -0,0 +1,39 @@
+#include "privacy/net/common/cpp/public_metadata/public_metadata.h"
+
+#include <string>
+
+#include "google/protobuf/timestamp.proto.h"
+#include "privacy/net/common/proto/public_metadata.proto.h"
+
+namespace privacy::ppn {
+
+BinaryPublicMetadata PublicMetadataProtoToStruct(
+    const PublicMetadata& metadata) {
+  BinaryPublicMetadata binary_struct;
+  // If any fields in the struct change, we need to bump the version.
+  binary_struct.version = 1;
+  if (!metadata.service_type().empty()) {
+    binary_struct.service_type = metadata.service_type();
+  }
+
+  if (metadata.has_exit_location()) {
+    if (!metadata.exit_location().country().empty() &&
+        metadata.exit_location().country().size() == 2) {
+      binary_struct.country.value()[0] = metadata.exit_location().country()[0];
+      binary_struct.country.value()[1] = metadata.exit_location().country()[1];
+    }
+    if (!metadata.exit_location().city_geo_id().empty()) {
+      binary_struct.region = metadata.exit_location().city_geo_id();
+    }
+  }
+
+  if (metadata.has_expiration()) {
+    binary_struct.expiration_epoch_seconds = metadata.expiration().seconds();
+  }
+  if (metadata.debug_mode() == PublicMetadata::DEBUG_ALL) {
+    binary_struct.debug_mode = 1;
+  }
+  return binary_struct;
+}
+
+}  // namespace privacy::ppn
---
+++ common/cpp/public_metadata/public_metadata.h
@@ -0,0 +1,42 @@
+#ifndef PRIVACY_NET_COMMON_CPP_PUBLIC_METADATA_PUBLIC_METADATA_H_
+#define PRIVACY_NET_COMMON_CPP_PUBLIC_METADATA_PUBLIC_METADATA_H_
+
+#include <array>
+#include <cstdint>
+#include <optional>
+#include <string>
+
+#include "privacy/net/common/proto/public_metadata.proto.h"
+
+namespace privacy::ppn {
+
+struct BinaryPublicMetadata {
+  // Incrememented with each version of this struct to allow for safe evolution
+  // of the struct.
+  uint32_t version;
+  // A fixed string for each service type that needs differentiation at the
+  // server.
+  std::optional<std::string> service_type;
+  // 2 character Country or Alpha2Code as defined by rfc8805 Section 2.1.1.2
+  std::optional<std::array<char, 2>> country;
+  // Usually 5 character Region like US-CA (California) as defined by rfc8805
+  // Section 2.1.1.3
+  std::optional<std::string> region;
+  // UTF-8 city name as defined by rfc8805 Section 2.1.1.4
+  std::optional<std::string> city;
+  // Seconds since epoch, rounded to the next 15 minute cutoff.
+  std::optional<uint64_t> expiration_epoch_seconds;
+  // copybara:strip_begin(doc is internal)
+  // See go/beryllium-geo-override for details.
+  // copybara:strip_end
+  // Indicates the debug context of this payload.
+  // 0 is UNSPECIFIED, and 1 is DEBUG_ALL.
+  uint32_t debug_mode;
+};
+
+BinaryPublicMetadata PublicMetadataProtoToStruct(
+    const PublicMetadata& metadata);
+
+}  // namespace privacy::ppn
+
+#endif  // PRIVACY_NET_COMMON_CPP_PUBLIC_METADATA_PUBLIC_METADATA_H_
