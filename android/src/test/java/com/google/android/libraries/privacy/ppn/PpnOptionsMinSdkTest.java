24202928bc3a4437f6b7a74ab6e840f7c00567ae
Googler <noreply@google.com>
2023-10-03 10:45:23 -0700

    No public description
    PiperOrigin-RevId: 570433344

--- android/src/test/java/com/google/android/libraries/privacy/ppn/PpnOptionsMinSdkTest.java
+++ android/src/test/java/com/google/android/libraries/privacy/ppn/PpnOptionsMinSdkTest.java
@@ -14,18 +14,22 @@
 
 package com.google.android.libraries.privacy.ppn;
 
+import static android.os.Build.VERSION_CODES.O_MR1;
+import static android.os.Build.VERSION_CODES.P;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.android.libraries.privacy.ppn.internal.KryptonConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 /** Unit tests for {@link PpnOptions}. */
 @RunWith(RobolectricTestRunner.class)
 public class PpnOptionsMinSdkTest {
   @Test
-  public void createKryptonConfig_ipsecProtocolInPpnOptions() {
+  @Config(sdk = O_MR1)
+  public void createKryptonConfig_ipsecProtocolInPpnOptionsApiLevel27() {
     PpnOptions options =
         new PpnOptions.Builder().setDatapathProtocol(PpnOptions.DatapathProtocol.IPSEC).build();
 
@@ -34,6 +38,19 @@ public class PpnOptionsMinSdkTest {
     // The Android SDK being used does not support IpSecManager so it should default to Bridge.
     assertThat(config.hasDatapathProtocol()).isTrue();
     assertThat(config.getDatapathProtocol()).isEqualTo(KryptonConfig.DatapathProtocol.BRIDGE);
+  }
+
+  @Test
+  @Config(sdk = P)
+  public void createKryptonConfig_ipsecProtocolInPpnOptionsApiLevel28() {
+    PpnOptions options =
+        new PpnOptions.Builder().setDatapathProtocol(PpnOptions.DatapathProtocol.IPSEC).build();
+
+    KryptonConfig config = options.createKryptonConfigBuilder().build();
+
+    // The Android SDK being used supports IpSecManager so it should use IPsec.
+    assertThat(config.hasDatapathProtocol()).isTrue();
+    assertThat(config.getDatapathProtocol()).isEqualTo(KryptonConfig.DatapathProtocol.IPSEC);
   }
 
   @Test
